0a49e50539fbc8bb47054ba7842a7e422d89edc2
samvritha8-santhosh <samvrithasanthosh8@gmail.com>
2026-06-04 10:38:20 +0530

    Update config.h

--- task03/day03/ manual_irrigation_toggle/config.h
+++ task03/day03/ manual_irrigation_toggle/config.h
@@ -1,1 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+const uint8_t BTN_MANUAL  = 2;
+const uint8_t LED_STATUS  = 13;
+const uint8_t RELAY_VALVE = 8;
+
+const bool RELAY_ACTIVE_LOW = true;
+
+#endif
