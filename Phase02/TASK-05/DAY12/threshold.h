581695355b0f892334b76b9d81a756887d7a3474
samvritha8-santhosh <samvrithasanthosh8@gmail.com>
2026-06-11 14:33:07 +0530

    Update threshold.h

--- Phase02/TASK-05/DAY12/threshold.h
+++ Phase02/TASK-05/DAY12/threshold.h
@@ -1,1 +1,13 @@
+#ifndef THRESHOLDS_H
+#define THRESHOLDS_H
 
+const int SOIL_MIN = 25;
+const int SOIL_TARGET = 40;
+
+const int LOW_COUNT_REQ = 3;
+const int HUMIDITY_MAX = 85;
+
+const unsigned long MAX_RUN_TIME = 10000UL;
+const unsigned long COOLDOWN_TIME = 5000UL;
+
+#endif
